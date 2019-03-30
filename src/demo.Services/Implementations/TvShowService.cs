7cc40ed48ebd9088d2e45b6f0564695f89c4b86f
egbertn <egbert_nierop@outlook.com>
2019-03-30 06:43:10 +0100

    refactors

--- src/demo.Services/Implementations/TvShowService.cs
+++ src/demo.Services/Implementations/TvShowService.cs
@@ -12,38 +12,36 @@ namespace demo.Services.Implementations
     public class TvShowService : ITvShowService
     {
         private readonly TvMazeClient _tvMazeClient;
+        private const int PAGE_SIZE = 50;
         // other option was to use InMemorySql but this is much more easy
         public TvShowService(TvMazeClient tvMazeClient)
         {
             _tvMazeClient = tvMazeClient;
         }
-        private int CacheKey(int pageNumber, int pageSize)
-        {
-            return  ((long)pageNumber | (long)pageSize << 32).GetHashCode();
-        }
-        public async Task<TvsShowsResponse> GetShowsWithActorAsync(int pageNumber, int pageSize = 50)
+      
+        public async Task<TvsShowsResponse> GetShowsWithActorAsync(int pageNumber, int pageSize = PAGE_SIZE)
         {
             if (pageNumber < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(pageNumber), "Must be greater or equal to 0");
             }
-            if (pageSize > 250)
+            if (pageSize > TvMazeClient.PAGE_SIZE)
             {
-                throw new ArgumentOutOfRangeException(nameof(pageSize), "Must not exceed 250");
+                throw new ArgumentOutOfRangeException(nameof(pageSize), $"Must not exceed {(double)TvMazeClient.PAGE_SIZE}");
             }
-            if (pageSize % 50 != 0)
+            if (pageSize % PAGE_SIZE != 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(pageSize), "must be dividable by 50");
+                throw new ArgumentOutOfRangeException(nameof(pageSize), $"must be dividable by {PAGE_SIZE}");
             }
 
 
-            var virtualPageSize = (pageNumber * pageSize) / 250D;
+            var virtualPageSize = (pageNumber * pageSize) / (double)TvMazeClient.PAGE_SIZE;
             var newTvShows = await _tvMazeClient.GetShowsAsync((int)Math.Floor(virtualPageSize));
 
 
-            var virtualSkip = ((pageNumber) * pageSize) % 250;
+            var virtualSkip = ((pageNumber) * pageSize) % TvMazeClient.PAGE_SIZE;
             var response = new TvsShowsResponse(newTvShows.Skip(virtualSkip)
-                // could be done using AutoMapper  for the sake of '3 hours' I skip this :)
+                // could be done using AutoMapper
                 .Select(s => new TvShow()
                 {
                     TvShowId = s.Id,
